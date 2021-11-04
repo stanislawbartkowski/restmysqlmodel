61e17a3592a9dcd339f6a9de9dbec2739a40b8e0
sbartkowski <stanislawbartkowski@gmail.com>
2021-11-04 18:39:49 +0100

    capital letter in TEST

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 source env.rc
-JAVA=/usr/lib/jvm/jre-15-openjdk/bin/java
+#JAVA=/usr/lib/jvm/jre-15-openjdk/bin/java
+JAVA=java
 JAR=target/restmysqlmodel-1.0-SNAPSHOT-jar-with-dependencies.jar
 $JAVA -cp $JAR:$JDBC RestMain -c $PROP -p $PORT
