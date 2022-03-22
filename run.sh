baed3a8820da091de0204c0dc7a7267b28d35c11
Your Name <you@example.com>
2022-03-22 01:22:04 +0100

    antd

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ source env.rc
 #JAVA=/usr/lib/jvm/jre-15-openjdk/bin/java
 JAVA=java
 JAR=target/restmysqlmodel-1.0-SNAPSHOT-jar-with-dependencies.jar
-$JAVA -cp $JAR:$JDBC RestMain -c $PROP -p $PORT
+$JAVA -Djava.util.logging.config.file=logging.properties -cp $JAR:$JDBC RestMain -c $PROP -p $PORT
